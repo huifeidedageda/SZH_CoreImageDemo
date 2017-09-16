22d07454fe642e0dc63afe97d4ced8d8be703e8a
SongZhiheng <szh13598845390@126.com>
2017-09-16 15:16:32 +0800

    初始化了项目

---
+++ SZH_CoreImageDemo/SZH_CoreImageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SZH_CoreImageDemo
+//
+//  Created by 智衡宋 on 2017/9/16.
+//  Copyright © 2017年 智衡宋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
